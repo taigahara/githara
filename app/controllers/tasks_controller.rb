b1f1f6a083de69349ab60040450012042599dad2
harataiga <harataiga1@MacBook-Air-2.local>
2021-09-23 23:11:05 +0900

    second commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,23 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params）
+    if @task.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :body)
+  end
 end
